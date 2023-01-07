6fa2581872f4e120328a8fffc6f9e2946a84a99d
MacBook Pro <>
2023-01-07 23:27:19 +0700

    revise port 3000 -> 7314

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ RUN npm install
 
 COPY . .
 CMD npm start
-EXPOSE 3000
+EXPOSE 7314
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const jwt = require('express-jwt');
 const axios = require('axios');
 
 const app = express()
-const port = 3000
+const port = 7314
 
 const SECRET = 'changeme';
 
